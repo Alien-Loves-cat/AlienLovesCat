e3c48a5ba768404fce8fd217842e360836ea40cf
Mooni <rlaansgml20@sookmyung.ac.kr>
2024-05-27 17:39:13 +0900

    일단 올려놔,,, inventory

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Item : ScriptableObject
 {
+    public enum ItemType {
+        Fruit,
+        Equipment,
+        Used,
+        Ingredient,
+        ETC,
+    }
     public string itemName;
+    public ItemType itemType;
     public Sprite itemImage;
+    public GameObject itemPrefab;
 }
